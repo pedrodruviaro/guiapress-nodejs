ceccd73234492f2638603fb30e0737cf2f716ba5
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-27 09:35:31 -0300

    adicionando rota de POST de categoria

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
+const slugify = require("slugify");
+const Category = require("../models/Category");
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
+});
+
+router.post("/categories/save", async (req, res) => {
+    const { title } = req.body;
+
+    if (title == undefined || title.trim() === "") {
+        return res.redirect("/admin/categories/new");
+    }
+
+    try {
+        await Category.create({
+            title,
+            slug: slugify(title),
+        });
+        return res.redirect("/");
+    } catch (error) {
+        return res.status(500).json(error);
+    }
 });
 
 module.exports = router;
