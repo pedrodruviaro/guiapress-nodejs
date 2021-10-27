76ea88e595caca1b0b9293130944f81d7dce5517
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-27 10:14:59 -0300

    adicionando confirmacao de apagar

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const slugify = require("slugify");
 const Category = require("../models/Category");
 
+// Renderizando pagina de adicao
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
 });
 
+// Inserindo uma categoria
 router.post("/categories/save", async (req, res) => {
     const { title } = req.body;
 
@@ -24,6 +26,7 @@ router.post("/categories/save", async (req, res) => {
     }
 });
 
+//Renderizando pagina de listagem
 router.get("/admin/categories", async (req, res) => {
     try {
         const categories = await Category.findAll();
@@ -32,6 +35,23 @@ router.get("/admin/categories", async (req, res) => {
     } catch (error) {
         return res.status(500).json(error);
     }
+});
+
+// Deletando uma categoria
+router.post("/categories/delete", async (req, res) => {
+    const id = req.body.id;
+
+    if (id == undefined || isNaN(id)) {
+        return res.status(400).redirect("/admin/categories");
+    }
+
+    await Category.destroy({
+        where: {
+            id: id,
+        },
+    });
+
+    return res.redirect("/admin/categories");
 });
 
 module.exports = router;
