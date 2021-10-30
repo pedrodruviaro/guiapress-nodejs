3782ee219ffcb31c2daf33ef9e72c50639d5cad5
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-30 09:34:52 -0300

    inserindo artigos e exibindo lista em tela

--- controllers/ArticlesController.js
+++ controllers/ArticlesController.js
@@ -1,7 +1,47 @@
 const router = require("express").Router();
+const Category = require("../models/Category");
+const Article = require("../models/Article");
+const slugify = require("slugify");
 
-router.get("/admin/articles/new", (req, res) => {
-    res.render("admin/articles/new");
+// pagina de artigos
+router.get("/admin/articles", async (req, res) => {
+    try {
+        const articles = await Article.findAll();
+
+        return res.render("admin/articles/index", { articles });
+    } catch (err) {
+        throw Error(err);
+    }
+});
+
+// renderizando pagina de criacao
+router.get("/admin/articles/new", async (req, res) => {
+    try {
+        const categories = await Category.findAll();
+
+        return res.render("admin/articles/new", { categories });
+    } catch (err) {
+        return res.redirect("/admin/articles/new");
+    }
+});
+
+// inserindo uma categoria
+router.post("/artcicles/save", async (req, res) => {
+    const { title, body, category } = req.body;
+
+    // categoryId => campo gerado ao fazer o relacionamento entre artigo e categoria (chave estrangeira)
+    try {
+        await Article.create({
+            title,
+            body,
+            categoryId: category,
+            slug: slugify(title),
+        });
+
+        return res.redirect("/admin/articles");
+    } catch (error) {
+        return res.redirect("/admin/articles");
+    }
 });
 
 module.exports = router;
